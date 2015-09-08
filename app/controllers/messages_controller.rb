01bdfc18b86c087359f0ddbac51776e4c448bd18
weezing <weezing@protonmail.com>
2015-09-08 22:41:58 +0200

    added forms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
 	def index
-		@messages = Message.all.order(created_at DESC)
+		@messages = Message.all.order("created_at DESC")
 	end
 
 	def new
