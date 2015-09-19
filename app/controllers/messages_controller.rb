5ff49919d009a1e90fe46341e15cd37af4a920a2
weezing <weezing@protonmail.com>
2015-09-19 10:54:07 +0200

    dodane destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -34,6 +34,7 @@ class MessagesController < ApplicationController
 	end
 
 	def destroy
+		@message = Message.find(params[:id])
 		@message.destroy
 		redirect_to root_path, notice: "Wiadomość usunięta!"
 	end
